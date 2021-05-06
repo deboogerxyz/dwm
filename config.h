0ffc89672e1c349b3f95956be5ce8148dda8778d
debooger <debooga@gmail.com>
2021-05-06 15:17:28 +0200

    Made borders 2px wide

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static unsigned int gappih    = 4;        /* horiz inner gap between windows */
 static unsigned int gappiv    = 4;        /* vert inner gap between windows */
