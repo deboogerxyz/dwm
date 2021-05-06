06ea79e1ebdebfcf78cb02f52e42e886495a18ac
debooger <debooga@gmail.com>
2021-05-06 21:18:40 +0200

    Added lf keybind

--- config.h
+++ config.h
@@ -148,6 +148,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD(TERMINAL " -e lf") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
